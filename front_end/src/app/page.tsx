415c164102e58e34b79e50bbfd959a1d66896ed0
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-02-13 13:34:00 -0300

    added: home_page basic styles

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -2,30 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-row">
 
-      <nav>
-        <div>
+      <nav className="bg-red-800 h-svh flex flex-col gap-4 px-6 py-3">
+        <div className="flex justify-center items-center gap-2">
           <img src="#" />
-          <p>username</p>
+          <p className="text-base">username</p>
         </div>
 
-        <a href="#">Home</a>
+        <a href="#" className="text-xl">Home</a>
 
         <ul>
-          <h3>My Books List</h3>
+          <h3 className="text-xl">My Books List</h3>
           <li>Read</li>
           <li>To Read</li>
           <li>Reading</li>
         </ul>
 
         <ul>
-          <h3>My Bookshelfs</h3>
+          <h3 className="text-xl">My Bookshelfs</h3>
           <li>2024 Books</li>
           <li>2025 Books</li>
         </ul>
 
-        <a href="#">Find Users</a>
+        <a className="text-xl" href="#">Find Users</a>
 
       </nav>
 
